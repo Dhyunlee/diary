6bf0c4dee081f710e77dd7dd5c70632136b779c6
Dhyunlee <skyblue5030@naver.com>
2023-04-10 17:05:39 +0900

    Chore: Base 폴더 추가

---
+++ src/components/Base/Label/styles.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const LabelWrap = styled.div`
+  width: 100px;
+  margin-bottom: 5px;
+  border-left: 3px solid #dea79e;
+  border-bottom: 1px solid lightgray;
+
+  span {
+    display: inline-block;
+    padding-left: 3px;
+    font-size: 15px;
+    font-weight: 400;
+  }
+`;
