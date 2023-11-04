3317ff7718be8af5f46308c74f4708e9679f88db
hyun0509 <skyblue5030@gmail.com>
2023-11-05 00:10:19 +0900

    fix: 로그인 상태인데 로그아웃 표시되는 현상 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Routers from "./routers";
 import { authService } from "./fbconfig";
 import DiaryTemplate from "./layouts/DiaryTemplate";
@@ -13,8 +13,9 @@ import { AppDispatch } from "store/configureStore";
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
   const { loadUserInfo } = useSelector(getUserState);
+  console.log(loadUserInfo)
   const dispatch = useDispatch<AppDispatch>();
-  
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
@@ -26,51 +27,47 @@ const App = () => {
     });
   }, [dispatch]);
 
-  const ComponentIsNotLogged = () => {
-    if (isLoggedIn === false) {
-      console.log("인증 필요");
-      return (
-        <div
-          style={{
-            height: "70vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: 18,
-            textAlign: "center",
-          }}
-        >
-          <p>다이어리를 사용하시려면 인증이 필요합니다.</p>
-        </div>
-      );
+  const render = () => {
+    if (isLoggedIn === null) {
+      console.log("loading", { isLoggedIn });
+      return <Spinners type="bar" color="#424242" loading={true} />;
+    } else {
+      if (isLoggedIn) {
+        console.log("로그인함", { isLoggedIn });
+        return (
+          <>
+            <Header isLoggedIn={isLoggedIn} />
+            <DiaryTemplate>
+              <Routers isLoggedIn={isLoggedIn} />
+            </DiaryTemplate>
+          </>
+        );
+      } else {
+        console.log("로그아웃", { isLoggedIn });
+        return (
+          <>
+            <Header isLoggedIn={isLoggedIn} />
+            <DiaryTemplate>
+              <div
+                style={{
+                  height: "70vh",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: 18,
+                  textAlign: "center",
+                }}
+              >
+                <p>다이어리를 사용하시려면 인증이 필요합니다.</p>
+              </div>
+            </DiaryTemplate>
+          </>
+        );
+      }
     }
   };
-  return (
-    <Wrap>
-      <>
-        {isLoggedIn && loadUserInfo === undefined ? (
-          <Spinners
-            type="bar"
-            color="#424242"
-            loading={loadUserInfo}
-          />
-        ) : (
-          <div>
-            <Header isLoggedIn={isLoggedIn as boolean} />
-            <>
-              <DiaryTemplate>
-                {isLoggedIn ? (
-                  <Routers isLoggedIn={isLoggedIn} />
-                ) : (
-                  ComponentIsNotLogged()
-                )}
-              </DiaryTemplate>
-            </>
-          </div>
-        )}
-      </>
-    </Wrap>
-  );
+
+  return <Wrap>{render()}</Wrap>;
 };
 
 export default App;
